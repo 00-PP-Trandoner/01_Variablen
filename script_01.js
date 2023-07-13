ebe07c1945f6017ef4caa81227c48146a7882776
Trandoner <trand_oner@gmx.de>
2023-07-13 11:45:28 +0200

    Variable mit KONSTANTEN Inhalt

--- script_01.js
+++ script_01.js
@@ -15,14 +15,14 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-// let firstName;
-// firstName = "Max";
-//console.log(firstName);
+// let firstName;                  //<-- Deklaration 
+// firstName = "Max";              //<--Wertzuweisung
+//console.log(firstName);          //<--Ausgabe
 
-// let familyName;                //<-- Deklaration 
+// let familyName;                 //<-- Deklaration 
 // familyName = "Mustermann";     //<--Wertzuweisung
-// console.log(familyName);      //<--Ausgabe 
-
+// console.log(familyName);       //<--Ausgabe 
+ 
 /***** 03 Deklaration + Wertzuweisung II *******/
 
 // let firstName, familyName;
@@ -33,11 +33,15 @@
 // console.log(firstName + " " + familyName);
 
 /* JS ist eine untypisierte Sprache! | untyped */
-let test;
-test = "Max";
-test = 2;
-test = false;           //<-- Die letzte Angabe wird übernommen !!!
+// let test;
+// test = "Max";
+// test = 2;
+// test = false;           //<-- Die letzte Angabe wird übernommen !!!
 
 
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+
+const firstName = "Max";      // Variable mit KONSTANTEN Inhalt 
+firstName = "Maximus";       // KEINE neue Zuweisung zur LZ möglich --> Fehler 
+console.log(firstName);      // AUGABE
