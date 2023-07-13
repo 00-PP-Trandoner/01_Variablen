802dba4a24129da2958231d45c6e3209cb0489ef
Trandoner <trand_oner@gmx.de>
2023-07-13 10:38:08 +0200

    Update

--- script_01.js
+++ script_01.js
@@ -15,3 +15,19 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+// let firstName;
+// firstName = "Max";
+//console.log(firstName);
+
+// let familyName; // Deklaration 
+// familyName = "Mustermann"; 
+// console.log(familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+let firstName, familyName;
+
+firstName = prompt("Bitte Vornamen eingeben");
+familyName = prompt("Bitte Nachnamen eingeben");
+
+console.log(firstName + " " + familyName);
